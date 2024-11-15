29523d22226e42bd049d4aedbe84df4e1a934374
ilyamikhailov <79221803601me@gmail.com>
2024-11-16 00:09:22 +0300

    Изменения

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("Cat");
+        Cat cat2 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean == bean1);
+        System.out.println(cat1 == cat2);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -3,12 +3,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-public class AppConfig {
- 
-    @Bean(name="helloworld")
+public class    AppConfig {
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Scope("prototype")
+    @Bean(name = "Cat")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
     }
 }
